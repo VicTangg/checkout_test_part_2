15feb60802feb5ce04809a4e67227bdb88ae9c42
Victor.Tang <victortang.com@gmail.com>
2022-06-28 11:27:27 +0800

    1

--- routes/api/webhooks.js
+++ routes/api/webhooks.js
@@ -6,10 +6,12 @@ var mySecretKey = 'sk_test_66d5b639-23bb-4ffa-ac0d-26a309fa8923';
 
 router.get('/payment-captured', (req, res) => {
     console.log(req)
+    res.json('');
 });
 
 router.post('/payment-captured', (req, res) => {
     console.log(req)
+    res.json('');
 });
 
 
