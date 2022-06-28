a29f0d6d9630c378ee5b27411b7c06e8845597ef
Victor.Tang <victortang.com@gmail.com>
2022-06-28 10:31:36 +0800

    a

--- routes/api/webhooks.js
+++ routes/api/webhooks.js
@@ -4,6 +4,9 @@ const router = express.Router();
 
 var mySecretKey = 'sk_test_66d5b639-23bb-4ffa-ac0d-26a309fa8923';
 
+router.get('/payment-captured', (req, res) => {
+    console.log(req)
+});
 
 router.post('/payment-captured', (req, res) => {
     console.log(req)
